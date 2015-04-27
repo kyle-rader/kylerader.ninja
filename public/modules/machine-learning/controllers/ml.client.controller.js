c7aaf3da18866b067f8d4a313599a8a28e070d74
kyle-rader <kyle.rader@ieee.org>
2015-04-27 03:24:52 -0700

    Add inclusion of Google's API.  Start graphing functions.  Parsing data correctly.

--- public/modules/machine-learning/controllers/ml.client.controller.js
+++ public/modules/machine-learning/controllers/ml.client.controller.js
@@ -4,8 +4,9 @@ angular.module('machine-learning').controller('MlController', ['$scope',
 	function($scope) {
 
 		$scope.buildData = function() {
-			$scope.loading = false;
+			$scope.loading = undefined;
 			$scope.error = undefined;
+
 			if ($scope.rawData === undefined || $scope.rawData === '') {
 				$scope.error = "I need dat raw data, yo.";
 				return;
@@ -16,12 +17,71 @@ angular.module('machine-learning').controller('MlController', ['$scope',
 
 			$scope.loading = true;
 
-			
+			var rawData = parseData($scope, $scope.rawData);
+			var clusterData = parseData($scope, $scope.clusterData);
+			if (rawData.length !== clusterData[0].length) {
+				$scope.error = "N in the raw data does not match N in the cluster data.";
+				$scope.loading = false;
+				return;
+			}
 
-		}
+			google.load('visualization', '1.1', {packages:['scatter']});
+			google.setOnLoadCallback(drawChart);
+			$scope.loading = undefined;
+		};
+		function drawChart() {
+			console.log('Calling draw Chart');
+			var data = google.visualization.arrayToDataTable([
+				['Age', 'Weight'],
+				[ 8,      12],
+				[ 4,      5.5],
+				[ 11,     14],
+				[ 4,      5],
+				[ 3,      3.5],
+				[ 6.5,    7]
+			]);
 
-		$scope.parseData = function(inputString) {
+			var options = {
+				title: 'Age vs. Weight comparison',
+				hAxis: {title: 'Age', minValue: 0, maxValue: 15},
+				vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
+				legend: 'none'
+			};
 
+			var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+			chart.draw(data, options);
+		};
+
+		function parseData($scope, inputString) {
+			var rows = inputString.trim().split(/\n/g);
+			var data = [];
+			var oldRowLength = null;
+
+			for(var i = 0; i < rows.length; i++) {
+				var rowData = rows[i].trim().split(/\s+/g);
+				var intRowData = [];
+				var rowLength = rowData.length;
+				if (i === 0) oldRowLength = rowLength;
+				if (oldRowLength !== rowLength) {
+					$scope.error = "Too many items found in a row.";
+					return null;
+				} else {
+					oldRowLength = rowLength;
+				}
+
+				/* Get numbers for the row */
+				for(var j = 0; j < rowLength; j++) {
+					var num = parseFloat(rowData[j]);
+					if (isNaN(num)) {
+						$scope.error = "Only numbers yo.";
+						return null;
+					}
+					intRowData.push(num);
+				}
+
+				data.push(intRowData);
+			}
+			return data;
 		}
 	}
 ]);

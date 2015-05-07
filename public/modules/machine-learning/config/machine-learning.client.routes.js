cd55186c8a0417ed972cb9a91dec369d2a27c735
kyle-rader <kyle.rader@ieee.org>
2015-05-07 13:39:15 -0700

    Add new machine learning route for KMeans.

--- public/modules/machine-learning/config/machine-learning.client.routes.js
+++ public/modules/machine-learning/config/machine-learning.client.routes.js
@@ -5,6 +5,10 @@ angular.module('machine-learning').config(['$stateProvider',
 	function($stateProvider) {
 		// Machine learning state routing
 		$stateProvider.
+		state('ml-kmeans', {
+			url: '/ml-kmeans',
+			templateUrl: 'modules/machine-learning/views/ml-kmeans.client.view.html'
+		}).
 		state('ml', {
 			url: '/ml',
 			templateUrl: 'modules/machine-learning/views/ml.client.view.html'
---
+++ public/modules/machine-learning/controllers/ml-kmeans.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('machine-learning').controller('MlKmeansController', ['$scope',
+	function($scope) {
+		// Controller Logic
+		// ...
+	}
+]);
